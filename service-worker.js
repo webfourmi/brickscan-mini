b383b6a40b81131e07a2359fe5de3e831c009ccb
webfourmi <134513115+webfourmi@users.noreply.github.com>
2026-09-05 14:52:38 +0200

    V1.3: bump PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,27 @@
-const CACHE = "brickscan-mini-v1.2.1";
+const CACHE = "brickscan-mini-v1.3";
 const ASSETS = [
   "./",
   "index.html",
   "styles.css",
   "data.js",
   "app.js",
-  "camera-hotfix.js",
   "manifest.webmanifest",
   "icons/icon-192.png",
   "icons/icon-512.png"
 ];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        if (response && response.ok && new URL(event.request.url).origin === location.origin) {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(cached => cached || caches.match("./")))
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    if (response && response.ok && new URL(event.request.url).origin === location.origin) {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./"))));
 });
