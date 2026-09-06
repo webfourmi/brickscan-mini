061bebe9fb000b413d672c556451b5e11ab50fac
webfourmi <134513115+webfourmi@users.noreply.github.com>
2026-09-06 13:11:59 +0200

    V2.1: bump backup version

--- backup.js
+++ backup.js
@@ -1,5 +1,5 @@
 (() => {
-  const APP_VERSION = '2.0.2';
+  const APP_VERSION = '2.1.0';
   const AUTO_KEY = 'brickscan-autobackup-v2';
   const AUTO_PREV_KEY = 'brickscan-autobackup-prev-v2';
   const $ = id => document.getElementById(id);
